a2c8704a62eafc8297d82a4f19337f4d3fea3272
Robert Posner <avianlord18@aim.com>
2025-01-14 22:02:36 -0800

    Finished loops assignment had problems with 2nd and 3rd steps

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        String grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+        
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else if (percentage <= 60)
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grafe is {grade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You Fail");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       //Console.Write("What is the magic number? ");
+        //int Magicnumber = int.Parse(Console.ReadLine());   
+
+        Random randomGenerator = new Random();
+        int Magicnumber = randomGenerator.Next(1, 100);
+        int guess = -1;
+      
+
+        while (Magicnumber != guess)
+        {
+       Console.WriteLine("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (Magicnumber == guess)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else if (guess > Magicnumber)
+        {
+            Console.WriteLine("Too High");
+        }
+        else
+        {
+            Console.WriteLine("Too low!");
+        }
+
+
+        }
     }
 }
+ 
