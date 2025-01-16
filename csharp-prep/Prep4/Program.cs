8df6b6ed446b3952b1339918c077021d17450f98
Robert Posner <avianlord18@aim.com>
2025-01-15 23:42:58 -0800

    Having trouble learning

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine("Total Sum: " + sum);
+
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine("average: " + average);
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        
+        }
+        {
+            Console.WriteLine("Max" + max);
+            
+        }
     }
 }
